089f32e365ba14a587be959a7dc74f0527efedbb
Totaku <admin@totaku.su>
2021-11-30 18:18:40 +0300

    edit task scripts

--- task/scripts.js
+++ task/scripts.js
@@ -8,7 +8,7 @@ const notify = require('gulp-notify');
 const babel = require('gulp-babel'); 
 const webpack = require('webpack-stream');
 
-const js = () =>{
+module.exports = function scripts() {
     return src(path.js.src, { sourcemaps: true })
     .pipe(plumber({
         errorHandler: notify.onError(error => ({
@@ -20,5 +20,3 @@ const js = () =>{
     .pipe(webpack(param.webpack))
     .pipe(dest(path.js.dest, { sourcemaps: '.' }));
 }
-
-module.exports = js;
