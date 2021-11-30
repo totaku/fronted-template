4d0130f791872883b89ecf4b7829c7f73f6675b5
Totaku <banana@3lf.ru>
2021-11-30 15:41:38 +0300

    add task clear

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ const { watch, series, parallel} = require('gulp');
 
 const path = require('./config/path');
 
+const clear =require('./task/clear');
 // const html = require('./task/html');
 const pug = require('./task/pug');
 
 // exports.html = html;
 exports.pug = pug;
+exports.clear = clear;
