1e8f89fc637eaa83d57ab8a95ef2df461b287f6c
Totaku <banana@3lf.ru>
2021-11-30 15:46:38 +0300

    add browser-sync, task server and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const { watch, series, parallel} = require('gulp');
+const browserSync = require('browser-sync').create();
 
 const path = require('./config/path');
 
@@ -6,6 +7,27 @@ const clear =require('./task/clear');
 // const html = require('./task/html');
 const pug = require('./task/pug');
 
+const server = () =>{
+    browserSync.init({
+        server:{ baseDir: path.root },
+        notify: false,
+        open: true,
+        cors: true,
+        browser: "FirefoxDeveloperEdition",
+        port: 3000
+    });
+}
+
+const watcher = () =>{
+    watch(path.pug.watch, pug).on('all', browserSync.reload); 
+}
+
 // exports.html = html;
 exports.pug = pug;
 exports.clear = clear;
+
+exports.dev = series(
+    clear,
+    pug,
+    parallel(watcher, server)
+)
